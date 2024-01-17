399ae524e5d43e4977f54945ce310629764a311d
Bsheskee <bartlomiejbialobrzewski@gmail.com>
2024-01-17 19:34:11 +0100

    Initial Commit

---
+++ CredProtector/CredProtectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CredProtectorApp.swift
+//  CredProtector
+//
+//  Created by bartek on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CredProtectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
